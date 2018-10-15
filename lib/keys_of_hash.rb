13d175a77ee08de55cf1ad9e07889971f93bc9ae
Sam B <s77broz@aol.com>
2018-10-15 17:22:57 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,7 @@
 class Hash
   def keys_of(*arguments)
-    out = []
-    self.keys.each do |key|
-      if arguments.include?(self[key])
-        out << key
-      end
+    self.keys.select do |key|
+      arguments.include?(self[key])
     end
-    out
   end
 end
